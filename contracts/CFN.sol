6e3732a15acf7e5cd23c08a4f858a1a8e19dea19
Inty Mendoza <intymendoza@gmail.com>
2022-07-19 13:48:26 -0300

    cfn.sol update

--- contracts/CFN.sol
+++ contracts/CFN.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
-
 contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
     using Address for address;
     using Strings for uint256;
@@ -25,7 +24,7 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
     mapping(address => uint256) private _balances;    
     mapping(uint256 => address) private _tokenApprovals;    
     mapping(address => mapping(address => bool)) private _operatorApprovals;
-
+    mapping(uint256 => string) private _tokenUri;
     
     event _stateBefore(address from,address to, uint256 tokenId);
     event _stateAfter(address previousOwner, address newOwner, uint256 tokenId);
@@ -35,57 +34,45 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
         require(isContractOwner(), "Function accessible only by contract owner!");
         _;
     }
-
     function isContractOwner() internal view returns (bool) {
         return msg.sender == _contractOwner;
-    }
-
-    
+    }    
     constructor() {
         _contractOwner = msg.sender;
     }
-
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
         return
             interfaceId == type(IERC721).interfaceId ||
             interfaceId == type(IERC721Metadata).interfaceId ||
             super.supportsInterface(interfaceId);
     }
-
     function balanceOf(address owner) public view virtual override returns (uint256) {
         require(owner != address(0), "ERC721: balance query for the zero address");
         return _balances[owner];
     }
-
     function ownerOf(uint256 tokenId) public view virtual override returns (address) {
         address owner = _owners[tokenId];
         require(owner != address(0), "ERC721: owner query for nonexistent token");
         return owner;
     }
-
-
     function name() public view virtual override returns (string memory) {
         return _name;
     }
-
-
     function symbol() public view virtual override returns (string memory) {
         return _symbol;
     }
-
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-
         string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+        string memory tokenUri = tokenMetadata(tokenId);
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenUri)) : "";
     }
-
-
     function _baseURI() internal view virtual returns (string memory) {
-        return "/ipfs/QmV1zRDqJipuPmVhxfhZ3Ve8XEYVACPGmZHJkgicxNFcrw";
+        return "/ipfs/";
     }
-
-
+    function tokenMetadata(uint256 tokenId) internal view virtual returns (string memory) {
+        return _tokenUri[tokenId];
+    }
     function approve(address to, uint256 tokenId) public virtual override {
         address owner = CommFutureNFT.ownerOf(tokenId);
         require(to != owner, "ERC721: approval to current owner");
@@ -97,72 +84,34 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
 
         _approve(to, tokenId);
     }
-
-
     function getApproved(uint256 tokenId) public view virtual override returns (address) {
         require(_exists(tokenId), "ERC721: approved query for nonexistent token");
 
         return _tokenApprovals[tokenId];
     }
-
-
     function setApprovalForAll(address operator, bool approved) public virtual override {
         _setApprovalForAll(_msgSender(), operator, approved);
     }
-
-
     function isApprovedForAll(address owner, address operator) public view virtual override returns (bool) {
         return _operatorApprovals[owner][operator];
     }
-
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {        
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override {        
         require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-
         _transfer(from, to, tokenId);
     }
-
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {
+    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
         safeTransferFrom(from, to, tokenId, "");
     }
-
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) public virtual override {
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {
         require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
         _safeTransfer(from, to, tokenId, _data);
     }
-
-
-    function _safeTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) internal virtual {
+    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory _data) internal virtual {
         require(from != address(0));            
         _transfer(from, to, tokenId);
         require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
     }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual {
+    function _transfer(address from, address to, uint256 tokenId) internal virtual {
         require(CommFutureNFT.ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
         require(to != address(0), "ERC721: transfer to the zero address");
         _beforeTokenTransfer(from, to, tokenId);       
@@ -172,38 +121,25 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
         _owners[tokenId] = to;
         emit Transfer(from, to, tokenId);
         _afterTokenTransfer(from, to, tokenId);
-    }
-    
+    }    
     function _exists(uint256 tokenId) internal view virtual returns (bool) {
         return _owners[tokenId] != address(0);
     }
-
-
     function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
         require(_exists(tokenId), "ERC721: operator query for nonexistent token");
         address owner = CommFutureNFT.ownerOf(tokenId);
         return (spender == owner || isApprovedForAll(owner, spender) || getApproved(tokenId) == spender);
-    }
-
-      
-    function safeMint(address to) public onlyContractOwner {
-        _tokenIdCounter.increment();
-        uint256 tokenId = _tokenIdCounter.current();        
+    }      
+    function safeMint(address to, string memory tokenUri) public onlyContractOwner {
+        _tokenIdCounter.increment();        
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenUri[tokenId] = tokenUri;        
         _safeMint(to, tokenId, "");
-    }
-   
-    function _safeMint(
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) internal virtual {
+    }   
+    function _safeMint(address to, uint256 tokenId, bytes memory _data) internal virtual {
         _mint(to, tokenId);
-        require(
-            _checkOnERC721Received(address(0), to, tokenId, _data),
-            "ERC721: transfer to non ERC721Receiver implementer"
-        );
+        require(_checkOnERC721Received(address(0), to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
     }
-
     function _mint(address to, uint256 tokenId) internal virtual {
         require(to != address(0), "ERC721: mint to the zero address");
         require(!_exists(tokenId), "ERC721: token already minted");
@@ -213,13 +149,11 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
         emit Transfer(address(0), to, tokenId);
         _afterTokenTransfer(address(0), to, tokenId);
     }
-
     function burn(uint256 tokenId) public {
         address owner = CommFutureNFT.ownerOf(tokenId);
         require(msg.sender == owner, "Only Token owner can burn it");        
         _burn(tokenId);
     }
-
     function _burn(uint256 tokenId) internal virtual {
         address owner = CommFutureNFT.ownerOf(tokenId);
         _beforeTokenTransfer(owner, address(0), tokenId);
@@ -228,33 +162,17 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
         delete _owners[tokenId];
         emit Transfer(owner, address(0), tokenId);
         _afterTokenTransfer(owner, address(0), tokenId);
-    }
-
-     
- 
+    } 
     function _approve(address to, uint256 tokenId) internal virtual {
         _tokenApprovals[tokenId] = to;
         emit Approval(CommFutureNFT.ownerOf(tokenId), to, tokenId);
     }
-
-
-    function _setApprovalForAll(
-        address owner,
-        address operator,
-        bool approved
-    ) internal virtual {
+    function _setApprovalForAll(address owner, address operator, bool approved) internal virtual {
         require(owner != operator, "ERC721: approve to caller");
         _operatorApprovals[owner][operator] = approved;
         emit ApprovalForAll(owner, operator, approved);
     }
-
-
-    function _checkOnERC721Received(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) private returns (bool) {
+    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data) private returns (bool) {
         if (to.isContract()) {
             try IERC721Receiver(to).onERC721Received(_msgSender(), from, tokenId, _data) returns (bytes4 retval) {
                 return retval == IERC721Receiver.onERC721Received.selector;
@@ -271,20 +189,10 @@ contract CommFutureNFT is Context, ERC165, IERC721, IERC721Metadata {
             return true;
         }
     }
-
-        function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual {
         emit _stateBefore(from, to, tokenId);
-    }
-    
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual {
+    }    
+    function _afterTokenTransfer(address from, address to, uint256 tokenId) internal virtual {
         emit _stateAfter(from, to, tokenId);
     }
 }
